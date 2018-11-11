7ed9dfdb54b67224ea4b756b59a33b0e366b1982
Kortnev Dmitriy <kortnevdmitriy@outlook.com>
2018-11-11 10:48:16 +0300

    Refactored by renaming.

--- app/src/main/java/ai/kortnevdmitriy/statusfocus/ui/SignIn.kt
+++ app/src/main/java/ai/kortnevdmitriy/statusfocus/ui/SignIn.kt
@@ -8,7 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class Signin : Fragment() {
+class SignIn : Fragment() {
 	
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
